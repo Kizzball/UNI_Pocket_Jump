61317b1eeb7976d99820d8e6dca4acd681950227
Kizzball <kikakifordu@gmail.com>
2025-10-16 11:15:31 +0100

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,173 +4,173 @@
 
 #define SCREEN_WIDTH 960
 #define SCREEN_HEIGHT 600
-#define PLATFORM_WIDTH 700
-#define PLATFORM_HEIGHT 100
+#define PLATFORM_WIDTH 100
+#define PLATFORM_HEIGHT 20
 #define PLAYER_WIDTH 30 
-#define PLAYER_HEIGHT 50
+#define PLAYER_HEIGHT 30
 #define GRAVITY 300
 #define JUMP_FORCE -400
-#define MAX_PLATFORM 10
+#define MAX_PLATFORMS 10
 
-
-typedef struct {
-    Rectangle rect;
+    typedef struct {
+        Rectangle rect;
+        float timeLeft;
         bool active;
     } Platform;
-
-typedef struct{
-    Rectangle rect;
-    Vector2 velocity;
+    
+    typedef struct {
+        Rectangle rect;
+        Vector2 velocity;
     } Player;
     
-    Platform platforms[MAX_PLATFORM];
+    Platform platforms[MAX_PLATFORMS];
     Player player;
-    float score = 0;
+    float score = 0 - 26;
     int highestPlatformY = SCREEN_HEIGHT;
     bool gameOver = false;
-
-void initGame();
-void UpdateGame(float dt);
-void DrawGame();
-void DrawGameOver();
-void ResetPlatform(Platform* platform);
-
-int main() {     
-    const int screenWidth = 960; //Screen Width
-    const int screenHeight = 600; //Screen Height
-
-    InitWindow(screenWidth, screenHeight, "pocket jump");
     
-     
-    SetTargetFPS(60);// the frame rate
+    void InitGame();
+    void UpdateGame(float dt);
+    void DrawGame();
+    void DrawGameOver();
+    void ResetPlatform(Platform* platform);
     
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        float dt = GetFrameTime();
-        // Update
-        UpdateGame(dt);
-        // Draw
+    
+    int main (){
+        InitWindow(SCREEN_WIDTH,SCREEN_HEIGHT,"Pocket Jump");
         
-        BeginDrawing();
+        InitGame();
+        
+        SetTargetFPS(60);
+        
+        while (!WindowShouldClose()){
+            
+            float dt = GetFrameTime();
+            
+            
+            UpdateGame(dt);
+            
+            BeginDrawing();
             ClearBackground(RAYWHITE);
+            
             if (gameOver){
-                DrawGameOver();// making the the game over screen
-            } else{
-                DrawGame();
+                DrawGameOver();
             }
-           
-        EndDrawing();
+                else{
+                    DrawGame();
+                }
+                EndDrawing();
+            }
+            CloseWindow();
+            return 0;
+            
+    }
+    
+    void InitGame(){
         
-    }
-
-    // De-Initialization
-    
-    CloseWindow();        // Close window and OpenGL context
-    
-
-    return 0;}
-
-void initGame(){
-   // starting point and moving speed when standing still
-    player.rect = Rectangle{SCREEN_WIDTH / 2- PLAYER_WIDTH /2,40, PLATFORM_WIDTH, PLAYER_HEIGHT};
-    player.velocity =Vector2{0,0};
-
-    //platform creation
-    for(int i = 0; i < MAX_PLATFORM; i++) {
-        ResetPlatform(&platforms[i]); //RANDOM CORDS
-        platforms[i].rect.y =SCREEN_HEIGHT - (i + 1) * 100; //vetical placement
-        platforms[i].active =true ; //actived all platforms????
-    }
-    //Reseting scores and state in the game
-    score = 0;
-    highestPlatformY = SCREEN_HEIGHT;
-    gameOver =false;
-    }
-
-
-
-//Function to update the game logic each frame???????
-void UpdateGame(float dt){
-//player jump code 
-if (IsKeyPressed(KEY_SPACE) && player.velocity.y == 0) //jump only if on floor and press space
-{player.velocity.y = JUMP_FORCE;}
-
-//Player controls code
-if (IsKeyDown(KEY_LEFT)) player.rect.x -= 200 * dt;
-if (IsKeyDown(KEY_RIGHT)) player.rect.x += 200 * dt;
-
-//Gravity for the player
-player.velocity.y += GRAVITY *dt;
-
-//Update the player's position based on velocity?????
-player.rect.y += player.velocity.y *dt;
-player.rect.x += player.velocity.x *dt;
-
-//remove unseen platforms?!?!------- maybe broken
-//If (platform.rect > SCREEN_HEIGHT ){platforms[i].active = false;}
-
-//collsions with platforms for player
-for (int i = 0; i < MAX_PLATFORM; i++) {
-    if (platforms[i].active && CheckCollisionRecs(player.rect,platforms[i].rect) && player.velocity.y > 0){
-    // stops player phasing
-    player.velocity.y = 0;
-    player.rect.y = platforms[i].rect.y - PLAYER_HEIGHT;} //player position adjustment?????
-    }
-}
-//ending screen when fall
-    0if (player.rect.y > SCREEN_HEIGHT){
-        gameOver = true;
-}
-
-//Scroll feature
-if (player.rect.y < SCREEN_HEIGHT / 2){
-    float offset = SCREEN_HEIGHT / 2 - player.rect.y;
-    player.rect.y +=- offset;
-    score += offset / 10;
-    
-    for (int i = 0; i < MAX_PLATFORM; i++){
-        platforms[i].rect.y += offset;
-        // reset platforms if they move off the bottom of the screen
-        if(platforms[i].rect.y > SCREEN_HEIGHT){
+        player.rect = Rectangle{SCREEN_WIDTH / 2 - PLAYER_WIDTH / 2, 40, PLAYER_WIDTH, PLAYER_HEIGHT};
+        player.velocity = Vector2{0, 0};
+        
+        for (int i = 0; i < MAX_PLATFORMS; i++){
             ResetPlatform(&platforms[i]);
-            platforms[i].rect.y =highestPlatformY - 100; 
-            highestPlatformY -= 100;
-
-        }
+            platforms[i].rect.y = SCREEN_HEIGHT - (i + 1) * 100;
+            platforms[i].active = true;
+            }
+        
+        score = 0;
+        highestPlatformY = SCREEN_HEIGHT;
+        gameOver = false;
     }
-
-}
-void DrawGame(){
-    DrawRectangleRec(player.rect, RED);
-
-    //Makes the platform appearance
-    for (int i = 0; i < MAX_PLATFORM; i++){
-        if(platforms[i].active){
-            DrawRectangleRec(platforms[i].rect, DARKGRAY);
+        void UpdateGame(float dt){
+            
+            if (IsKeyPressed(KEY_SPACE) && player.velocity.y == 0){
+            player.velocity.y = JUMP_FORCE;
+            }
+            if (IsKeyPressed(KEY_UP) && player.velocity.y == 0){
+            player.velocity.y = JUMP_FORCE;
+            }
+            
+            if (IsKeyDown(KEY_LEFT)) player.rect.x -= 200 * dt;
+            if (IsKeyDown(KEY_RIGHT)) player.rect.x += 200 * dt;
+            
+            player.velocity.y += GRAVITY * dt;
+            
+            player.rect.y += player.velocity.y * dt;
+            player.rect.x += player.velocity.x * dt;
+            
+            for(int i = 0; i < MAX_PLATFORMS; i++) {
+                if (platforms[i].active && CheckCollisionRecs(player.rect, platforms[i].rect) && player.velocity.y > 0){
+                    
+                    player.velocity.y = 0;
+                    player.rect.y = platforms[i].rect.y - PLATFORM_HEIGHT;
+                    
+                    platforms[i].timeLeft -= dt;
+                    if(platforms[i].timeLeft <= 0){
+                        platforms[i].active = false;
+                    }
+                }
+            }
+            
+            if (player.rect.y > SCREEN_HEIGHT){
+                gameOver = true;
+            }
+            
+            if (player.rect.y < SCREEN_HEIGHT / 2){
+                float offset = SCREEN_HEIGHT / 2 - player.rect.y;
+                player.rect.y += offset;
+                score += offset / 10;
+                
+                for(int i = 0; i < MAX_PLATFORMS; i++){
+                    platforms[i].rect.y += offset;
+                    
+                    if(platforms[i].rect.y > SCREEN_HEIGHT){
+                        ResetPlatform(&platforms[i]);
+                        platforms[i].rect.y = highestPlatformY - 100;
+                        highestPlatformY -= 100;
+                        
+                    }
+                }
+            }
         }
-    }
-    //score text & placing
-    DrawText(TextFormat("Score: %.0f", score),10,10,20, BLACK);
-}
-void ResetPlatform(platform* platform){
-    //randomizes platforms cords
-    platform->rect =Rectangle{
-        (float)GetRandomValue(0,SCREEN_WIDTH -PLATFORM_WIDTH),
-        (float)GetRandomValue(-200,0),
-        (float)PLATFORM_WIDTH,
-        (float)PLATFORM_HEIGHT,
+        
+        void DrawGame(){
+            DrawRectangleRec(player.rect, BROWN);
+            
+            for(int i = 0; i < MAX_PLATFORMS; i++){
+                if(platforms[i].active){
+                    DrawRectangleRec(platforms[i].rect, BLUE);
+                }
+            }
+            
+            DrawText(TextFormat("Current Score: %.0f", score), 10, 10, 20, BLACK);
+            
+        }
+                    
+            
+            
+        void ResetPlatform(Platform* platform){
+            
+            platform->rect = Rectangle{
+                (float)GetRandomValue(0, SCREEN_WIDTH -PLATFORM_WIDTH),
+                (float)GetRandomValue(-200, 0),
+                (float)PLATFORM_WIDTH,
+                (float)PLATFORM_HEIGHT
+                
+            };
+            platform->timeLeft = 5.0f;
+            platform->active = true;
+            
+        }
+        void DrawGameOver(){
+            DrawText("Game Over", SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2 -50, 40, RED);
+            DrawText(TextFormat("Final Score: %.0f", score), SCREEN_WIDTH /2 -100, SCREEN_HEIGHT / 2, 20, BLACK);
+            DrawText("Press R to Try Again", SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2 + 50, 20 ,DARKGRAY);
+            
+            if (IsKeyPressed(KEY_R)){
+                InitGame();
+            }
+            
+        }
     
-    };
-}
-// the "Game Over" display
-void DrawGameOver(){
-    DrawText("GAME OVER you failed",SCREEN_WIDTH / 2 -100,SCREEN_HEIGHT / 2 - 50, 40, RED);
-    DrawText(TextFormat("Score:%.0f", score), SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2, 20, BLACK);
-    DrawText("Press R to TRY AGAIN",SCREEN_WIDTH / 2 -100,SCREEN_HEIGHT / 2 + 50, 20, DARKGRAY);
-
-    //restart button
-    if (IsKeyPressed(KEY_R)){
-        initGame();
-    }
-}
+        
+        
